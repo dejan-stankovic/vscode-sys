85f24d0f6591fa4354621a93f67624da034d3ac8
Darin Morrison <darinmorrison@users.noreply.github.com>
2019-04-15 14:48:52 -0700

    Sorting

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,9 @@
 pub(crate) mod commands;
-pub(crate) mod extension_context;
 pub(crate) mod disposable;
+pub(crate) mod extension_context;
 pub(crate) mod window;
 
 pub use commands::{commands, Commands};
-pub use extension_context::ExtensionContext;
 pub use disposable::Disposable;
+pub use extension_context::ExtensionContext;
 pub use window::{window, Window};

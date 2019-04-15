7c829aae3bfcee7d4fdcf9335e2d4feecb01b7e4
Darin Morrison <darinmorrison@users.noreply.github.com>
2019-04-14 21:06:39 -0700

    Return the promise immediately

--- examples/hello/src/lib.rs
+++ examples/hello/src/lib.rs
@@ -1,18 +1,20 @@
-use js_sys::Promise;
+use js_sys::{Array, Promise};
 use vscode_sys;
 use wasm_bindgen::{prelude::*, JsCast};
 
 #[wasm_bindgen]
 pub fn activate(context: &vscode_sys::ExtensionContext) -> Promise {
     console_error_panic_hook::set_once();
-    let tag = "extension.helloWASM".into();
-    let clo = Closure::wrap(Box::new(|| {
-        vscode_sys::window.show_information_message("Hello from Rust!".into());
-    }) as Box<dyn FnMut()>);
-    let disposable = vscode_sys::commands.register_command(tag, clo.as_ref().unchecked_ref());
-    clo.forget(); // NOTE: leak the closure here so that it remains active
-    context.subscriptions().push(disposable.as_ref());
-    Promise::resolve(&JsValue::undefined())
+    Promise::new(&mut |resolve, _reject| {
+        let tag = "extension.helloWASM".into();
+        let clo = Closure::wrap(Box::new(|| {
+            vscode_sys::window.show_information_message("Hello from Rust!".into());
+        }) as Box<dyn FnMut()>);
+        let disposable = vscode_sys::commands.register_command(tag, clo.as_ref().unchecked_ref());
+        clo.forget(); // NOTE: leak the closure here so that it remains active
+        context.subscriptions().push(disposable.as_ref());
+        resolve.apply(&JsValue::null(), &Array::new()).unwrap();
+    })
 }
 
 #[wasm_bindgen]

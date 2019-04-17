9b60f2817d55d6c06ac5b0fc238c575d9e20eb84
Darin Morrison <darinmorrison@users.noreply.github.com>
2019-04-17 14:31:16 -0700

    Use the returned promise; refactor

--- examples/hello/src/lib.rs
+++ examples/hello/src/lib.rs
@@ -5,7 +5,7 @@
 
 use futures::{prelude::*, FutureExt};
 use futures_util::compat::Compat;
-use js_sys::Promise;
+use js_sys::{Array, Promise};
 use vscode_sys::ExtensionContext;
 use wasm_bindgen::{prelude::*, JsCast};
 use wasm_bindgen_futures::future_to_promise;
@@ -13,20 +13,30 @@ use wasm_bindgen_futures::future_to_promise;
 #[allow(non_snake_case)]
 async fn register_command_helloWASM(context: ExtensionContext) -> Result<JsValue, JsValue> {
     use vscode_sys::{commands, window};
-    let tag = "extension.helloWASM".into();
+    // invoked when the command "extension.helloWASM" is invoked
     let clo = Closure::<Fn()>::new(|| {
-        let msg = "Hello from Rust!".into();
-        window.show_information_message(msg);
+        // invoked when the "show_information_message" API call (below) finishes
+        let clo = Closure::<FnMut(JsValue)>::new(|res| {
+            let args = Array::new();
+            args.push(&"should be `undefined`:".into());
+            args.push(&res);
+            web_sys::console::log(&args);
+        });
+        window
+            .show_information_message("Hello from Rust!".into())
+            .then(&clo);
+        clo.forget();
     });
-    let fun = clo.as_ref().unchecked_ref();
-    context
-        .subscriptions()
-        .push(&commands.register_command(tag, fun));
+    context.subscriptions().push({
+        let tag = "extension.helloWASM".into();
+        let fun = clo.as_ref().unchecked_ref();
+        &commands.register_command(tag, fun)
+    });
     clo.forget();
     Ok(JsValue::undefined())
 }
 
-async fn register_commands(context: ExtensionContext) -> Result<JsValue, JsValue> {
+async fn activate_future(context: ExtensionContext) -> Result<JsValue, JsValue> {
     await!(future::join_all(vec![register_command_helloWASM(context)]));
     Ok(JsValue::undefined())
 }
@@ -34,7 +44,7 @@ async fn register_commands(context: ExtensionContext) -> Result<JsValue, JsValue
 #[wasm_bindgen]
 pub fn activate(context: ExtensionContext) -> Promise {
     console_error_panic_hook::set_once();
-    let future = register_commands(context);
+    let future = activate_future(context);
     let pinned = future.boxed();
     let compat = Compat::new(pinned);
     future_to_promise(compat) // FIXME: wasm-bindgen-futures still uses old API

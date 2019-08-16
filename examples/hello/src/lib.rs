9e38b3264f5b22f3bc449fe639af4abfe453cd30
Darin Morrison <darinmorrison@users.noreply.github.com>
2019-08-16 15:58:26 -0700

    Use refs after passing context to future

--- examples/hello/src/lib.rs
+++ examples/hello/src/lib.rs
@@ -3,12 +3,13 @@
 use futures::prelude::*;
 use futures_util::compat::Compat;
 use js_sys::{Array, Promise};
+use std::pin::Pin;
 use vscode_sys::ExtensionContext;
 use wasm_bindgen::{prelude::*, JsCast};
 use wasm_bindgen_futures::future_to_promise;
 
 #[allow(non_snake_case)]
-async fn register_command_helloWASM(context: ExtensionContext) -> Result<JsValue, JsValue> {
+async fn register_command_helloWASM(context: &ExtensionContext) -> Result<JsValue, JsValue> {
     use vscode_sys::{commands, window};
     // invoked when the command "extension.helloWASM" is invoked
     let clo = Closure::<dyn Fn()>::new(|| {
@@ -34,7 +35,8 @@ async fn register_command_helloWASM(context: ExtensionContext) -> Result<JsValue
 }
 
 async fn activate_future(context: ExtensionContext) -> Result<JsValue, JsValue> {
-    let futures = vec![register_command_helloWASM(context)];
+    let mut futures: Vec<Pin<Box<dyn Future<Output = Result<_, _>>>>> = Vec::new();
+    futures.push(Box::pin(register_command_helloWASM(&context)));
     future::try_join_all(futures).await?;
     Ok(JsValue::undefined())
 }
@@ -42,9 +44,7 @@ async fn activate_future(context: ExtensionContext) -> Result<JsValue, JsValue> 
 #[wasm_bindgen]
 pub fn activate(context: ExtensionContext) -> Promise {
     console_error_panic_hook::set_once();
-    let future = activate_future(context);
-    let pinned = Box::pin(future);
-    let compat = Compat::new(pinned);
+    let compat = Compat::new(Box::pin(activate_future(context)));
     future_to_promise(compat) // FIXME: wasm-bindgen-futures still uses old API
 }
 

d32407398876f822a2f93074588151b758b0299f
Darin Morrison <darinmorrison@users.noreply.github.com>
2019-11-22 21:23:24 -0700

    Disable compat feature; fix example

--- examples/hello/src/lib.rs
+++ examples/hello/src/lib.rs
@@ -1,5 +1,4 @@
 use futures::prelude::*;
-use futures_util::compat::Compat;
 use js_sys::{Array, Promise};
 use std::pin::Pin;
 use vscode_sys::ExtensionContext;
@@ -10,19 +9,19 @@ use wasm_bindgen_futures::future_to_promise;
 async fn register_command_helloWASM(context: &ExtensionContext) -> Result<JsValue, JsValue> {
     use vscode_sys::{commands, window};
     // invoked when the command "extension.helloWASM" is invoked
-    let clo = Closure::<dyn Fn()>::new(|| {
+    let clo = Closure::wrap(Box::new(|| {
         // invoked when the "show_information_message" API call (below) finishes
-        let clo = Closure::<dyn FnMut(JsValue)>::new(|res| {
+        let clo = Closure::wrap(Box::new(|res| {
             let args = Array::new();
             args.push(&"should be `undefined`:".into());
             args.push(&res);
             web_sys::console::log(&args);
-        });
+        }) as Box<dyn FnMut(JsValue)>);
         window
             .show_information_message("Hello from Rust!".into())
             .then(&clo);
         clo.forget();
-    });
+    }) as Box<dyn Fn()>);
     context.subscriptions().push({
         let tag = "extension.helloWASM".into();
         let fun = clo.as_ref().unchecked_ref();
@@ -42,8 +41,7 @@ async fn activate_future(context: ExtensionContext) -> Result<JsValue, JsValue> 
 #[wasm_bindgen]
 pub fn activate(context: ExtensionContext) -> Promise {
     console_error_panic_hook::set_once();
-    let compat = Compat::new(Box::pin(activate_future(context)));
-    future_to_promise(compat) // FIXME: wasm-bindgen-futures still uses old API
+    future_to_promise(Box::pin(activate_future(context))) // FIXME: wasm-bindgen-futures still uses old API
 }
 
 #[wasm_bindgen]
